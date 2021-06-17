f84afbd9d1b36eff5c1e7b78cd49664f36c13bf4
Teklehaimanot Belete <enbish.blt@gmail.com>
2021-06-17 11:34:20 +0300

    adding button to the Header

---
+++ src/component/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button= ()=> {
+    return (
+        <button>Add</button>
+    )
+}
+
+export default Button
+
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,11 @@
-const Header = ({title}) => {
+
+import Button from './Button'
+const Header = ({ title }) => {
     return (
-        <h1>{title}</h1>
+        <Header className="header">
+            <h1>{title}</h1>
+            <Button />
+        </Header>
     )
 }
 
